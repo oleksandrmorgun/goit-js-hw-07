bd6a13f2538bc31ae658eba8b13b6ba32fd8020c
oleksandrmorgun <127879303+oleksandrmorgun@users.noreply.github.com>
2023-07-24 11:35:24 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const containerGalleryItems = document.querySelector(".gallery");
+
+const createGalleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  )
+  .join("");
+
+containerGalleryItems.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+const galleryImages = document.querySelectorAll(".gallery__image");
+galleryImages.forEach((image) => {
+  image.addEventListener("click", onImgClick);
+});
+
+function onhandleEscapeKeydown(instance) {
+  return function (event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  };
+}
+
+function onImgClick(event) {
+  event.preventDefault();
+  const clickedElement = event.target;
+
+  if (clickedElement.classList.contains("gallery__image")) {
+    const instance = basicLightbox.create(
+      `<img src="${clickedElement.dataset.source}" width="800" height="600">`,
+      {
+        onShow: (instance) => {
+          document.addEventListener("keydown", handleEscapeKeydown);
+        },
+        onClose: (instance) => {
+          document.removeEventListener("keydown", handleEscapeKeydown);
+        },
+      }
+    );
+    const handleEscapeKeydown = onhandleEscapeKeydown(instance);
+    instance.show();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const containerGalleryItems = document.querySelector(".gallery");
+
+const createGalleryMarkyp = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+containerGalleryItems.insertAdjacentHTML("beforeend", createGalleryMarkyp);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
